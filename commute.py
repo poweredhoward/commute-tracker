86fe178d909b34f40bc6e6b3b607b07926fde0a6
Matthew <matthew.howard96@yahoo.com>
2019-02-11 22:20:43 -0800

    Add config file

--- commute.py
+++ commute.py
@@ -1,14 +1,28 @@
 import requests
 import pprint
 import json
+import sys
 
+import config as vars
+
+# print( vars.ENDPOINT +
+#     "?origin="+ vars.HOME +
+#     "?destination="+ vars.WORK +
+#     "?departure_time="+ vars.DEPARTURE_TIME + 
+#     "?key="+ vars.KEY)
+
+argDestination = sys.argv[1]
+print(argDestination)
+
+origin = vars.CULVER_CITY
+destination = vars.argDestination
 
 homeToWork = requests.get(
-    "https://maps.googleapis.com/maps/api/directions/json?" +
-    "origin=14851+W+Sunset+Blvd+90272&" +
-    "destination=540+W+Woodbury+Rd+91001&"+
-    "departure_time=now&"+
-    "key=AIzaSyAstItVHmWt5mJ4HSotWOSIT4s4Y_4UBfs"
+    vars.API_ENDPOINT +
+    "?origin="+ origin +
+    "&destination="+ destination +
+    "&departure_time="+ vars.DEPARTURE_TIME + 
+    "&key="+ vars.KEY
 )
 
 j = homeToWork.json()
---
+++ config.py
@@ -0,0 +1,10 @@
+API_ENDPOINT = "https://maps.googleapis.com/maps/api/directions/json"
+HOME = "14851+W+Sunset+Blvd+90272"
+WORK = "540+W+Woodbury+Rd+91001"
+CULVER_CITY = "4250+Irving+Culver+City+CA+90232"
+DEPARTURE_TIME = "now"
+KEY = "AIzaSyAstItVHmWt5mJ4HSotWOSIT4s4Y_4UBfs"
+
+
+
+# CONVERT TO JSON!!!!!!!!!
